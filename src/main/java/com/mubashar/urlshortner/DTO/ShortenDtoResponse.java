07495ba695fccf73463a9afce9b4059cc86457b6
unknown <imubashirhussen@gmail.com>
2026-08-09 22:29:37 +0500

    added a Response DTO

--- src/main/java/com/mubashar/urlshortner/DTO/ShortenDtoResponse.java
+++ src/main/java/com/mubashar/urlshortner/DTO/ShortenDtoResponse.java
@@ -3,9 +3,5 @@ package com.mubashar.urlshortner.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class ShortenDtoResponse {
-  private  String originalURl;
-   private String shortenUrl;
+public record ShortenDtoResponse (String originalUrl){
 }
