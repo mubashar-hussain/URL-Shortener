a53c3c860ef15631864684b4e3b15eabcc1af9a1
unknown <imubashirhussen@gmail.com>
2026-08-14 23:05:16 +0500

    re write some code

--- src/main/java/com/mubashar/urlshortner/Entity/Shortener.java
+++ src/main/java/com/mubashar/urlshortner/Entity/Shortener.java
@@ -1,10 +1,25 @@
 package com.mubashar.urlshortner.Entity;
 
-import jakarta.persistence.Entity;
-import org.springframework.stereotype.Service;
+import jakarta.persistence.*;
+import lombok.*;
 
 
 @Entity
+@Table(name = "short_url")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Shortener {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   @Column(nullable = false,length = 2048)
+    private String originalURl ;
+
+    @Column(nullable = false , unique = true)
+    private String shortUrl;
 
 }
