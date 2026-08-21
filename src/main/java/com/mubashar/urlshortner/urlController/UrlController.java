6fee10ac6413351eb3b03ed193ce67b98de26252
unknown <imubashirhussen@gmail.com>
2026-08-21 12:14:05 +0500

    refactor code

--- src/main/java/com/mubashar/urlshortner/urlController/UrlController.java
+++ src/main/java/com/mubashar/urlshortner/urlController/UrlController.java
@@ -1,32 +1,25 @@
 package com.mubashar.urlshortner.urlController;
 
-import com.mubashar.urlshortner.DTO.ShortenDtoRequest;
-import com.mubashar.urlshortner.DTO.ShortenDtoResponse;
-import com.mubashar.urlshortner.Entity.Shortener;
+import com.mubashar.urlshortner.DTO.ShortenUrlRequest;
+import com.mubashar.urlshortner.DTO.ShortenUrlResponse;
 import com.mubashar.urlshortner.urlShortnerService.UrlService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/urls")
-public class Url {
-private  final UrlService urlService;
+public class UrlController {
+    private final UrlService urlService;
 
 
     @PostMapping
-    public ResponseEntity<ShortenDtoResponse> creatOriginalUrl(ShortenDtoRequest shortenDtoRequest){
+    public ResponseEntity<ShortenUrlResponse> sendOriginalUrl(@RequestBody ShortenUrlRequest shortener){
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(urlService.creatOriginalUrl(shortenDtoRequest);
+        return ResponseEntity.ok(urlService.sendUrl(shortener));
     }
 
-    @GetMapping("/url/{id}")
-    public Optional<Shortener> getShortUrl(@PathVariable  Long id ){
-        return urlService.getShroteUrl(id);
 
-    }
 }
