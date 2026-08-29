de7d2da765f75923a262a6f99cf10ae4785271cc
unknown <imubashirhussen@gmail.com>
2026-08-29 13:05:43 +0500

    Added ExceptionHandler

---
+++ src/main/java/com/mubashar/urlshortner/customexception/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.mubashar.urlshortner.customexception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+}
