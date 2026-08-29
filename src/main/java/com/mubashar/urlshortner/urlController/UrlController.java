9d6b492a1773165ce41c6379e578c7f6ac9131db
unknown <imubashirhussen@gmail.com>
2026-08-29 12:21:23 +0500

    reduce some lines

--- src/main/java/com/mubashar/urlshortner/urlController/UrlController.java
+++ src/main/java/com/mubashar/urlshortner/urlController/UrlController.java
@@ -3,23 +3,38 @@ package com.mubashar.urlshortner.urlController;
 import com.mubashar.urlshortner.DTO.ShortenUrlRequest;
 import com.mubashar.urlshortner.DTO.ShortenUrlResponse;
 import com.mubashar.urlshortner.urlShortnerService.UrlService;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
+//@AllArgsConstructor
 @RequestMapping("/api/urls")
 public class UrlController {
     private final UrlService urlService;
 
+    public UrlController(UrlService urlService){
+        this.urlService = urlService;
 
-    @PostMapping
-    public ResponseEntity<ShortenUrlResponse> sendOriginalUrl(@RequestBody ShortenUrlRequest shortener){
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<ShortenUrlResponse> createShortUrl(@RequestBody ShortenUrlRequest shortener){
 
         return ResponseEntity.ok(urlService.sendUrl(shortener));
     }
 
+
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortCode) {
+
+        String  originalUrl = urlService.getOriginalUrl(shortCode);
+        return  ResponseEntity
+                .status(302)
+                .header("Location",originalUrl)
+                .build();
+
+    }
 
 }
